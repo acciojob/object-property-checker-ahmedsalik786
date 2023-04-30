2b5da0e665efb4deb1f66ae6b10dc9550b10b6fa
SALIK AHMED <92015547+ahmedsalik786@users.noreply.github.com>
2023-04-30 14:07:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-		if(sampleObject.includes(key)){
-        return true;
+for(let i in sampleObject){
+        if(key==i){
+            return true;
+        }
     }
-    else{
-        return false;
-    }
+    return false;
 	
 }
 
